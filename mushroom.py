b6e43b8736396d998fbf3aab9ba830b7a2a6fa49
elmisadr <99094352+elmisadr@users.noreply.github.com>
2023-05-14 13:06:27 +0200

    Update mushroom.py

--- mushroom.py
+++ mushroom.py
@@ -1,61 +1,75 @@
-import numpy as np
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 from tsetlinMachine import TsetlinMachine
+from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Load mushroom dataset
-url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data'
-mushroom_data = pd.read_csv(url, header=None)
-mushroom_data = mushroom_data.sample(frac=1).reset_index(drop=True) # shuffle rows
-X = mushroom_data.iloc[:, 1:]
-y = mushroom_data.iloc[:, 0]
 
-# One-hot encode X and y
-enc = OneHotEncoder()
-X = enc.fit_transform(X)
-y = np.array([1 if i == 'p' else 0 for i in y])
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Define the URL
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data"
 
-# Define Tsetlin Machine model
-# Convert sparse matrix to dense matrix
-X_train = X_train.toarray()
+# Define the column names
+col_names = ['class', 'cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor', 
+             'gill-attachment', 'gill-spacing', 'gill-size', 'gill-color', 'stalk-shape', 
+             'stalk-root', 'stalk-surface-above-ring', 'stalk-surface-below-ring', 
+             'stalk-color-above-ring', 'stalk-color-below-ring', 'veil-type', 'veil-color', 
+             'ring-number', 'ring-type', 'spore-print-color', 'population', 'habitat']
 
-# Define Tsetlin Machine model
-tm = TsetlinMachine(100, 2, 3.9,4,3)
+# Load the data
+df = pd.read_csv(url, header=None, names=col_names)
 
-# Train the model
-epochs = 100
-train_acc = []
-val_acc = []
-test_acc = []
-for epoch in range(epochs):
-    # Train on training set
-    tm.fit(X_train, y_train, X_train.shape[0], epochs=100)
-    train_accuracy = tm.evaluate(X_train, y_train, X_train.shape[0])
-    train_acc.append(train_accuracy)
-    
-    # Evaluate on validation set
-    val_accuracy = tm.evaluate(X_val, y_val, X_val.shape[0])
-    val_acc.append(val_accuracy)
-    
-    print("Epoch {}: train accuracy = {}, val accuracy = {}".format(epoch+1, train_accuracy, val_accuracy))
-    
-# Evaluate the model on the test set
-X_test = X_test.toarray() # convert sparse matrix to dense matrix
-test_accuracy = tm.evaluate(X_test, y_test, X_test.shape[0])
-print("Test accuracy = {}".format(test_accuracy))
+# Instantiate the encoders
+label_enc = LabelEncoder()
+one_hot_enc = OneHotEncoder()
 
-   
-    
-# Plot the accuracy curves
+# Encode target variable
+df['class'] = label_enc.fit_transform(df['class'])
+
+# One-hot encode the categorical features
+df_encoded = pd.get_dummies(df)
+
+# Separate features and target
+X = df_encoded.drop('class', axis=1).values
+y = df_encoded['class'].values
+
+# Split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Initialize your TsetlinMachine
+tm = TsetlinMachine(number_of_clauses=10, number_of_features=X_train.shape[1], number_of_states=10, s=3.9, threshold=15)
+alpha = tm.calculate_equation(s, decay_rate, epoch)
+tm.fit(X_train, y_train, number_of_examples=X_train.shape[0], epochs=100, alpha=alpha)
+
+# Predict on the training and test set
+y_train_pred = np.array([tm.predict(x) for x in X_train])
+y_test_pred = np.array([tm.predict(x) for x in X_test])
+
+# Calculate training and test accuracies
+train_acc = accuracy_score(y_train, y_train_pred)
+test_acc = accuracy_score(y_test, y_test_pred)
+
+# Calculate training and test precision
+train_prec = precision_score(y_train, y_train_pred)
+test_prec = precision_score(y_test, y_test_pred)
+
+# Calculate training and test recall
+train_recall = recall_score(y_train, y_train_pred)
+test_recall = recall_score(y_test, y_test_pred)
+
+# Print training and test accuracies
+print(f'Training accuracy: {train_acc:.2f}, Precision: {train_prec:.2f}, Recall: {train_recall:.2f}')
+print(f'Test accuracy: {test_acc:.2f}, Precision: {test_prec:.2f}, Recall: {test_recall:.2f}')
+
+# Plot the training and test accuracy curves
+plt.figure(figsize=(10, 6))
 plt.plot(train_acc, label='Train Accuracy')
 plt.plot(test_acc, label='Test Accuracy')
-plt.xlabel('Epoch')
+plt.title('Train and Test Accuracy Curves')
+plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
-plt.title('Accuracy Curves')
 plt.legend()
 plt.show()
